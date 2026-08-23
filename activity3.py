a7f18b80704942d6632222cbcb2d316ae947070f
Zeusrayne <manalozeusrayne@gmail.com>
2026-08-23 17:30:35 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,48 @@
+#int
+fam_member = 5
+age = 18 
+money = 300
+birthyear = 2008
+
+
+print("I have a total of" , fam_member , "family member")
+print("My age is" , age)
+print("My money is" , money)
+print("My birthyear is" , birthyear)
+
+#float
+temperature = 36.8
+height = 182.88
+weight = 68.7
+price = 7.53
+
+print("My temperature is" , temperature)
+print("My height is" , height)
+print("My weight is" , weight)
+print("The price of the martys is" , price)
+
+#string
+name = "Hulk"
+movie = "The Avengers"
+fav_hero = "Spiderman"
+best_movie = "The Amazing Spiderman 2"
+
+print("The first hero I know is" , name)
+print("I first watched Hulk in" , movie)
+print("My favorite hero is" , fav_hero)
+print("One of the best movie is" , best_movie)
+
+
+#boolean
+isreach = "TRUE"
+istall = "TRUE"
+issmall = "FALSE"
+ismuslim = "FALSE"
+
+print("I can reach the ceiling of my house:" , isreach)
+print("Im taller than my Mom:" , istall)
+print("My hands are small:" , issmall)
+print("My religion is muslim:" , ismuslim)
+
+
+
