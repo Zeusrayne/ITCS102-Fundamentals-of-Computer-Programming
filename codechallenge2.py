46e3fbc131e7d67cb7d46e60c8a35ac8816e95ed
Zeusrayne <manalozeusrayne@gmail.com>
2026-08-26 20:11:56 +0800

    Add files via upload

---
+++ codechallenge2.py
@@ -0,0 +1,13 @@
+#operator = symbol 
+
+money = 19863
+fd = 19863 // 1000
+fd2 = 863 // 500
+fd3 = 363 // 200
+fd4 = 163 // 100
+fd5 = 63 // 50
+fd6 = 13 // 10
+fd7 = 3 // 1
+print("The money to deposit-->" , money , "\n19000 =", fd , "\n800 =" , fd2 , "\n300 =" , fd3 , "\n100 =" , fd4 , "\n60 =" , fd5 , "\n10 =" , fd6 , "\n3 =" , fd7)
+
+
