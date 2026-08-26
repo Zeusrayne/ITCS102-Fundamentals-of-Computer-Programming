1a984e447295a97703dd8d74a808c98323f5f63c
Zeusrayne <manalozeusrayne@gmail.com>
2026-08-26 20:14:29 +0800

    Update codechallenge2.py

--- codechallenge2.py
+++ codechallenge2.py
@@ -8,6 +8,6 @@ fd4 = 163 // 100
 fd5 = 63 // 50
 fd6 = 13 // 10
 fd7 = 3 // 1
-print("The money to deposit-->" , money , "\n19000 =", fd , "\n800 =" , fd2 , "\n300 =" , fd3 , "\n100 =" , fd4 , "\n60 =" , fd5 , "\n10 =" , fd6 , "\n3 =" , fd7)
+print("The money to deposit-->" , money , "\n19000 =", fd , "\n800 =" , fd2 , "\n300 =" , fd3 , "\n100 =" , fd4 , "\n50 =" , fd5 , "\n10 =" , fd6 , "\n3 =" , fd7)
 
 
