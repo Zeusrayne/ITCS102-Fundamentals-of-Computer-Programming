c8ed751ec0a32be75018dfe7232d06332d7554e8
Zeusrayne <manalozeusrayne@gmail.com>
2026-09-04 11:06:53 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,33 @@
+total = 10 
+
+n = eval(input("enter any number -->"))
+total = total + n
+
+n = eval(input("enter any number -->"))
+total +=  n
+
+n = eval(input("enter any number -->"))
+total -= n
+
+n = eval(input("enter any number -->"))
+total *= n
+
+n = eval(input("enter any number -->"))
+total -= n
+
+n = eval(input("enter any number -->"))
+total += n
+
+n = eval(input("enter any number -->"))
+total += n
+
+n = eval(input("enter any number -->"))
+total -= n 
+
+n = eval(input("enter any number -->"))
+total *= n
+
+n = eval(input("enter any number -->"))
+total -= n 
+
+print ("Current Total is --->", total) 
