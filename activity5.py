ea33776c5878a816af870abb2bd6b47eb0ea130b
Zeusrayne <manalozeusrayne@gmail.com>
2026-09-11 10:23:38 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,11 @@
+#operators activity
+
+a = 17
+b = 8
+sum = a + b
+diff = a - b
+product = a*b
+
+print("The sum of", a, "and", b, "is", sum)
+print("The difference of", a, "and", b, "is", diff)
+print("The product of ", a, "and", b, "is", product)
