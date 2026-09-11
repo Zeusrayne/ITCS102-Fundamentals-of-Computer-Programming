260106d933ef1f742b01de9044b33913cb0ebbea
Zeusrayne <manalozeusrayne@gmail.com>
2026-09-11 10:27:15 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,17 @@
+# > - greater than
+# < - less than
+# >= greater than or equal to
+# <= less than or equal to
+# != not equal
+# == equal to
+
+x = 15
+y = 5
+
+
+
+print ( x > y )
+print ( x >= y )
+print ( x <= y )
+print ( x != y)
+print ( x == y)	
