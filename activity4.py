f7f42a8f994f0a039fe942a7ba322146e447f6ea
Zeusrayne <manalozeusrayne@gmail.com>
2026-09-11 10:20:32 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,13 @@
+#escape sequence
+# \n - new line
+# \t - tab
+# \r - carriage return/ deprecated/obsolete
+# \b - backspace
+# \\ - insert slash
+# \" - insert double quotation
+
+phrase1 = "\"no one'\bs can make me \nfeel \t this way"
+phrase2 = "love \"is like\t\n a movie"
+
+print(phrase1)
+print(phrase2)
